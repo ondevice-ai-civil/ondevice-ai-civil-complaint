9f2e92eafd1c057a6768f536c73a1ef99a9abb45
umyunsang <dbstkd5865@gmail.com>
2026-04-09 23:44:17 +0900

    fix: __version__ 이중 관리 해소 + 개인 이메일 제거
    - src/cli/__init__.py: 하드코딩된 __version__을 importlib.metadata로 변경
    - pyproject.toml: author email을 GitHub no-reply 이메일로 변경

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -1,3 +1,8 @@
 """GovOn CLI package."""
 
-__version__ = "1.0.1"
+try:
+    from importlib.metadata import version
+
+    __version__ = version("govon")
+except Exception:
+    __version__ = "0.0.0-dev"

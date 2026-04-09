92b69ada46f57d093bd715c8481c68430f2694f1
umyunsang <164823608+umyunsang@users.noreply.github.com>
2026-04-10 01:39:24 +0900

    fix: Homebrew formula 실제 PyPI sha256으로 교체 (#629)
    - 모든 placeholder sha256 → 실제 PyPI sdist sha256
    - govon 1.0.6 + 14개 dependency resource
    - bottle :unneeded 제거 (deprecated)
    - version 필드 제거 (url에서 자동 추출)
    - homepage URL 수정 (GovOn-Org)

--- homebrew/govon.rb
+++ homebrew/govon.rb
@@ -4,42 +4,82 @@
 class Govon < Formula
   include Language::Python::Virtualenv
 
-  desc "Shell-first local agentic runtime for administrative assistance and civil complaint workflows"
-  homepage "https://github.com/GovOn-org/GovOn"
-
-  # TODO: PyPI 배포 후 업데이트
-  url "https://files.pythonhosted.org/packages/source/G/GovOn/GovOn-1.0.1.tar.gz"
-  sha256 "# TODO: PyPI 배포 후 실제 sha256으로 업데이트"
+  desc "Agentic CLI shell for Korean public-sector civil complaint workflows"
+  homepage "https://github.com/GovOn-Org/GovOn"
+  url "https://files.pythonhosted.org/packages/63/45/775f9cdc742ab45a3e125a31f9a0aefd3a55909c5b94cf70c3b4f3236c9e/govon-1.0.6.tar.gz"
+  sha256 "79662f7ba9e0b7d6ba1dfe3acf31576298d73f25255ceaa9f2a0ab36ee69c3c3"
   license "MIT"
-  version "1.0.1"
-
-  bottle :unneeded
 
   depends_on "python@3.12"
 
-  # Core runtime dependencies (inference extras 제외, CLI 실행에 필요한 최소 의존성)
   resource "httpx" do
-    # TODO: PyPI 배포 후 업데이트
-    url "https://files.pythonhosted.org/packages/source/h/httpx/httpx-0.27.0.tar.gz"
-    sha256 "# TODO: 업데이트 필요"
+    url "https://files.pythonhosted.org/packages/b1/df/48c586a5fe32a0f01324ee087459e112ebb7224f646c0b5023f5e79e9956/httpx-0.28.1.tar.gz"
+    sha256 "75e98c5f16b0f35b567856f597f06ff2270a374470a5c2392242528e3e3e42fc"
+  end
+
+  resource "httpcore" do
+    url "https://files.pythonhosted.org/packages/06/94/82699a10bca87a5556c9c59b5963f2d039dbd239f25bc2a63907a05a14cb/httpcore-1.0.9.tar.gz"
+    sha256 "6e34463af53fd2ab5d807f399a9b45ea31c3dfa2276f15a2c3f00afff6e176e8"
+  end
+
+  resource "anyio" do
+    url "https://files.pythonhosted.org/packages/19/14/2c5dd9f512b66549ae92767a9c7b330ae88e1932ca57876909410251fe13/anyio-4.13.0.tar.gz"
+    sha256 "334b70e641fd2221c1505b3890c69882fe4a2df910cba14d97019b90b24439dc"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/af/2d/7bf41579a8986e348fa033a31cdd0e4121114f6bce2457e8876010b092dd/certifi-2026.2.25.tar.gz"
+    sha256 "e887ab5cee78ea814d3472169153c2d12cd43b14bd03329a39a9c6e2e80bfba7"
+  end
+
+  resource "h11" do
+    url "https://files.pythonhosted.org/packages/01/ee/02a2c011bdab74c6fb3c75474d40b3052059d95df7e73351460c8588d963/h11-0.16.0.tar.gz"
+    sha256 "4e35b956cf45792e4caa5885e69fba00bdbc6ffafbfa020300e549b208ee5ff1"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/6f/6d/0703ccc57f3a7233505399edb88de3cbd678da106337b9fcde432b65ed60/idna-3.11.tar.gz"
+    sha256 "795dafcc9c04ed0c1fb032c2aa73654d8e8c5023a7df64a53f39190ada629902"
+  end
+
+  resource "sniffio" do
+    url "https://files.pythonhosted.org/packages/a2/87/a6771e1546d97e7e041b6ae58d80074f81b7d5121207425c964ddf5cfdbd/sniffio-1.3.1.tar.gz"
+    sha256 "f4324edc670a0f49750a81b895f35c3adb843cca46f0530f79fc1babb23789dc"
   end
 
   resource "loguru" do
-    # TODO: PyPI 배포 후 업데이트
-    url "https://files.pythonhosted.org/packages/source/l/loguru/loguru-0.7.0.tar.gz"
-    sha256 "# TODO: 업데이트 필요"
+    url "https://files.pythonhosted.org/packages/3a/05/a1dae3dffd1116099471c643b8924f5aa6524411dc6c63fdae648c4f1aca/loguru-0.7.3.tar.gz"
+    sha256 "19480589e77d47b8d85b2c827ad95d49bf31b0dcde16593892eb51dd18706eb6"
   end
 
   resource "rich" do
-    # TODO: PyPI 배포 후 업데이트
-    url "https://files.pythonhosted.org/packages/source/r/rich/rich-13.0.0.tar.gz"
-    sha256 "# TODO: 업데이트 필요"
+    url "https://files.pythonhosted.org/packages/b3/c6/f3b320c27991c46f43ee9d856302c70dc2d0fb2dba4842ff739d5f46b393/rich-14.3.3.tar.gz"
+    sha256 "b8daa0b9e4eef54dd8cf7c86c03713f53241884e814f4e2f5fb342fe520f639b"
   end
 
-  resource "prompt_toolkit" do
-    # TODO: PyPI 배포 후 업데이트
-    url "https://files.pythonhosted.org/packages/source/p/prompt_toolkit/prompt_toolkit-3.0.0.tar.gz"
-    sha256 "# TODO: 업데이트 필요"
+  resource "pygments" do
+    url "https://files.pythonhosted.org/packages/c3/b2/bc9c9196916376152d655522fdcebac55e66de6603a76a02bca1b6414f6c/pygments-2.20.0.tar.gz"
+    sha256 "6757cd03768053ff99f3039c1a36d6c0aa0b263438fcab17520b30a303a82b5f"
+  end
+
+  resource "markdown-it-py" do
+    url "https://files.pythonhosted.org/packages/5b/f5/4ec618ed16cc4f8fb3b701563655a69816155e79e24a17b651541804721d/markdown_it_py-4.0.0.tar.gz"
+    sha256 "cb0a2b4aa34f932c007117b194e945bd74e0ec24133ceb5bac59009cda1cb9f3"
+  end
+
+  resource "mdurl" do
+    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
+    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+  end
+
+  resource "prompt-toolkit" do
+    url "https://files.pythonhosted.org/packages/a1/96/06e01a7b38dce6fe1db213e061a4602dd6032a8a97ef6c1a862537732421/prompt_toolkit-3.0.52.tar.gz"
+    sha256 "28cde192929c8e7321de85de1ddbe736f1375148b02f2e17edd840042b1be855"
+  end
+
+  resource "wcwidth" do
+    url "https://files.pythonhosted.org/packages/35/a2/8e3becb46433538a38726c948d3399905a4c7cabd0df578ede5dc51f0ec2/wcwidth-0.6.0.tar.gz"
+    sha256 "cdc4e4262d6ef9a1a57e018384cbeb1208d8abbc64176027e2c2455c81313159"
   end
 
   def install
@@ -47,7 +87,6 @@ class Govon < Formula
   end
 
   test do
-    # govon CLI 진입점 기본 동작 확인
-    system bin/"govon", "--help"
+    assert_match "govon", shell_output("#{bin}/govon --help", 2)
   end
 end
